3f8f1ca6f52ec81b5c210738b574ad4f770379da
Mario D'Anna <mario.danna@hcl.com>
2022-01-07 10:38:18 +0100

    set correct date and time in header

--- userwidgets/com.probtp.AppHeader/modules/require/AppHeaderController.js
+++ userwidgets/com.probtp.AppHeader/modules/require/AppHeaderController.js
@@ -1,6 +1,25 @@
 define(function() {
+  return {
+    constructor: function(baseConfig, layoutConfig, pspConfig) {
+      this.view.preShow = () => {
+        this.setDateTime();
+        if(!this.initDone){
+          kony.timer.schedule('updateDateTime', () => {
+            this.setDateTime();
+          }, 60, true);
+          this.initDone = true;
+        }
+      };
+    },
+    
+    initGettersSetters: function() {
 
-	return {
-
-	};
+    },
+    
+    setDateTime(){
+      const now = new Date();
+      this.view.lblDate.text = utils.formatDate(now);
+      this.view.lblTime.text = utils.formatTime(now);
+    }
+  };
 });

d49379d18733560574d7f9419c214ed6c8408e77
Mario D'Anna <mario.danna@hcl.com>
2022-01-12 15:43:41 +0100

    Finished first page

---
+++ userwidgets/com.probtp.StandardDateSelector/modules/require/StandardDateSelectorController.js
@@ -0,0 +1,23 @@
+define(function() {
+
+  return {
+    constructor: function(baseConfig, layoutConfig, pspConfig) {
+      this.view.preShow = () => {
+        const now = new Date();
+        this.view.calSelection.dateComponents = this.view.calSelection.dateComponents || [now.getDate(), now.getMonth() + 1, now.getFullYear(), 0, 0, 0];
+        this.view.imgSelection.onTouchEnd = () => this.view.calSelection.open();
+      };
+    },
+
+    initGettersSetters() {},
+
+    getDate(){
+      let ret = null;
+      const dateComp = this.view.calSelection.dateComponents;
+      if(dateComp){
+        ret = `${dateComp[0]}/${dateComp[1]}/${dateComp[2]}`;
+      }
+      return ret;
+    }
+  };
+});
